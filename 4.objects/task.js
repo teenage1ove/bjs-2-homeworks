8a9b3b8735749f62cedc553c3bc592139002191c
teenage1ove <kirillstars7@gmail.com>
2024-08-26 10:07:27 +0300

    Add homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.marks = []
 }
 
+const student = new Student('Alex', 'male', 22)
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks) {
+    this.marks = [...this.marks, ...marks]
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks && this.marks.length !== 0) {
+    return this.marks.reduce((a, b) => a + b, 0) / this.marks.length
+  }
+
+  return 0
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+
+  this.excluded = reason
 }
