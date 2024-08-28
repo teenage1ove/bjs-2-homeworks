5936c41bfeac61d4eeed41496caff34b06e666b0
teenage1ove <kirillstars7@gmail.com>
2024-08-28 12:21:15 +0300

    Обработка исключений и замыкания дз

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿function parseCount(count) {
+	const num = Number.parseFloat(count, 10)
+	if (Number.isNaN(num)) {
+		throw new Error('Невалидное значение')
+	} else {
+		return num
+	}
+}
+
+function validateCount(count) {
+	try {
+		return parseCount(count)
+	} catch (error) {
+		return error
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.sideA = a
+		this.sideB = b
+		this.sideC = c
+		if (
+			this.sideA + this.sideB < this.sideC ||
+			this.sideB + this.sideC < this.sideA ||
+			this.sideA + this.sideC < this.sideB
+		) {
+			throw new Error('Треугольник с такими сторонами не существует')
+		}
+	}
+
+	get perimeter() {
+		return this.sideA + this.sideB + this.sideC
+	}
+
+	get area() {
+		const p = this.perimeter / 2
+		const area = Math.sqrt(
+			p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)
+		)
+		return +area.toFixed(3)
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c)
+	} catch (e) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует'
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует'
+			}
+		}
+	}
+}
